dd6ff3301593cf056e2e5464aedc43766b306ee8
Zoe Thysell <zoethysell@gmail.com>
2021-05-17 18:11:25 +0200

    Made a script that randomizes skin tone

---
+++ Assets/_MageBall/Scripts/RandomizeSkinColor.cs
@@ -0,0 +1,26 @@
+using Mirror;
+using UnityEngine;
+
+namespace MageBall
+{
+    public class RandomizeSkinColor : NetworkBehaviour
+    {
+        [SerializeField] private new Renderer renderer;
+        [SerializeField] private int skinMaterialSlot;
+        [SerializeField] private Material[] skinMaterials;
+        [SyncVar(hook = nameof(OnCurrentSkinIndexChanged))]
+        private int currentSkinIndex = -1;
+
+        public override void OnStartServer()
+        {
+            currentSkinIndex = Random.Range(0, skinMaterials.Length);
+        }
+
+        private void OnCurrentSkinIndexChanged(int oldSkinIndex, int newSkinIndex)
+        {
+            Material[] materials = renderer.materials;
+            materials[skinMaterialSlot] = skinMaterials[newSkinIndex];
+            renderer.materials = materials;
+        }
+    }
+}
