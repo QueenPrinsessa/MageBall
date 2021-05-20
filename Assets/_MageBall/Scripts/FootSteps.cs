0b5e642cecf60d637d18270208a92c6239ff929e
Dangbaka99 <tonnyhua99@gmail.com>
2021-05-20 15:04:25 +0200

    Added sounds to footsteps

---
+++ Assets/_MageBall/Scripts/FootSteps.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+
+    [SerializeField] private AudioClip[] clips;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Step()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+    }
+
+
+}
